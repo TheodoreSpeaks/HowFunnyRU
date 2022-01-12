1cdeb876ed9379162b093bfd1b45e82f7a8b5a37
albearli <yiliang6@illinois.edu>
2022-01-11 18:59:43 -0600

    Add microphone logic to detect pauses

--- cheese/src/components/Microphone.tsx
+++ cheese/src/components/Microphone.tsx
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import NeonButton from "./NeonButton";
 
 export default function Microphone(): React.ReactElement {
-  const { transcript, listening, browserSupportsSpeechRecognition } =
-    useSpeechRecognition();
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const [startedRecording, setStartedRecording] = useState(false);
+  const [fullTranscript, setFullTranscript] = useState("");
+
+  useEffect(() => {
+    if (!listening && startedRecording) {
+      // Pause detected
+      setFullTranscript((fullTranscript) => fullTranscript + " " + transcript);
+      SpeechRecognition.startListening();
+    }
+  }, [listening]);
+
+  const startListening = (): void => {
+    SpeechRecognition.startListening();
+    setStartedRecording(true);
+  };
+
+  const stopListening = (): void => {
+    SpeechRecognition.stopListening();
+    setStartedRecording(false);
+  };
+
+  const resetFullTranscript = () => {
+    resetTranscript();
+    setFullTranscript("");
+    stopListening();
+  };
 
   if (!browserSupportsSpeechRecognition) {
     return <>Browser does not support speech recognition!</>;
   }
 
-  const startListening = (): void => {
-    SpeechRecognition.startListening({ continuous: true });
-  };
-
-  const stopListening = (): void => {
-    SpeechRecognition.stopListening();
-  };
-
   return (
     <div>
       <p>Microphone: {listening ? "on" : "off"}</p>
-      <div>
-        <NeonButton clickHandler={startListening}>Start</NeonButton>
-        <NeonButton clickHandler={stopListening}>Stop</NeonButton>
-      </div>
-      <p>{transcript}</p>
+      <NeonButton clickHandler={startListening}>Start</NeonButton>
+      <NeonButton clickHandler={stopListening}>Stop</NeonButton>
+      <NeonButton clickHandler={resetFullTranscript}>Reset</NeonButton>
+      <p>
+        {fullTranscript} {transcript}
+      </p>
     </div>
   );
 }
