fa5164c676e73b7fb67b30d6ef1a9290880fb074
Theodore Li <theodoreqili@gmail.com>
2022-01-13 00:30:23 -0600

    Add humor ranking on microphone pause

--- cheese/src/components/Microphone.tsx
+++ cheese/src/components/Microphone.tsx
@@ -3,6 +3,7 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import NeonButton from "./NeonButton";
+import GptConfig from "../gpt3.json";
 
 export default function Microphone(): React.ReactElement {
   const {
@@ -15,11 +16,17 @@ export default function Microphone(): React.ReactElement {
   const [startedRecording, setStartedRecording] = useState(false);
   const [fullTranscript, setFullTranscript] = useState("");
 
+  const OpenAI = require('openai-api');
+  const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+  const openai = new OpenAI(OPENAI_API_KEY);
+
   useEffect(() => {
     if (!listening && startedRecording) {
       // Pause detected
       setFullTranscript((fullTranscript) => fullTranscript + " " + transcript);
       SpeechRecognition.startListening();
+
+      requestTranscriptHumorScore();
     }
   }, [listening]);
 
@@ -38,6 +45,20 @@ export default function Microphone(): React.ReactElement {
     setFullTranscript("");
     stopListening();
   };
+
+  const requestTranscriptHumorScore = async () => {
+    if (transcript === "") return;
+
+    const updatedPrompt = GptConfig.prompt + "\nSentence: " + transcript + "\nScore:";
+
+    const configCopy = { ...GptConfig };
+    configCopy.prompt = updatedPrompt
+
+    const response = await openai.complete(configCopy);
+    const score = parseInt(response.data['choices'][0]['text']);
+
+    console.log(score);
+  }
 
   if (!browserSupportsSpeechRecognition) {
     return <>Browser does not support speech recognition!</>;
