5843b8e27733e83036140a68ebb79efca3496f8b
albearli <yiliang6@illinois.edu>
2022-01-14 13:53:18 -0600

    Add ranked laugh tracks based on score

--- cheese/src/util/useAudio.tsx
+++ cheese/src/util/useAudio.tsx
@@ -3,11 +3,12 @@
  * @param score humor score given by GPT3
  */
 export function playAudio(score: number): void {
-  const laughTrackNumber = Math.floor(Math.random() * 3);
-  const track = require(`../assets/laugh_${laughTrackNumber}.mp3`);
+  if (score === 0) return;
+
+  const track = require(`../assets/laugh_${score}.mp3`);
   const audio = new Audio(track);
 
   console.log("playing");
-  audio.volume = score / 10;
+  audio.volume = 0.5;
   audio.play();
 }
